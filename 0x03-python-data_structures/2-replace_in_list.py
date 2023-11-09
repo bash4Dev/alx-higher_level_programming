a0be8b3eeb980d4982e8542a67f564cdcc5b4cd5
Bashir Umar Idris <idrisbashirumr@gmail.com>
2023-11-09 04:12:39 -0800

    Modify 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 def replace_in_list(my_list, idx, element):
+    n = len(my_list)
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx > n - 1:
         return my_list
     else:
-        my_list = element
+        my_list[idx] = element
         return my_list
